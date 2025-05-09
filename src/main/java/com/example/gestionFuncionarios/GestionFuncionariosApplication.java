8503051e474d7730c54fe3517d8a97ef595abf77
Inigo1Gomez <inigobenjamin2425@gmail.com>
2025-05-09 12:22:58 -0400

    wdasd

---
+++ src/main/java/com/example/gestionFuncionarios/GestionFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFuncionariosApplication.class, args);
+	}
+
+}
